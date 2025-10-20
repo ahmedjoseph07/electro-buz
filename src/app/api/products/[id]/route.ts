9a218dd592ffee75cb4897c4679579695314bf3a
Joseph Ahmed <ahmedjoseph11@gmail.com>
2025-10-20 16:19:37 +0600

    deployment issue

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -2,27 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import Product, { ProductDoc } from "@/models/Product";
 
-interface Params {
-  params: { id: string };
-}
-
 export const GET = async (
   req: NextRequest,
-  context: Params
+  context: { params: { id: string } }
 ): Promise<NextResponse> => {
   try {
     await connectDB();
 
     const { id } = context.params;
-    const product = await Product.findById<ProductDoc>(id).lean();
+    const product = await Product.findById(id).lean<ProductDoc & { _id: string }>();
 
     if (!product) {
       return NextResponse.json({ message: "Product not found" }, { status: 404 });
     }
 
-    const productWithIdString = { ...product, _id: product._id.toString() };
-
-    return NextResponse.json(productWithIdString);
+    return NextResponse.json(product);
   } catch (error) {
     console.error("Error fetching product:", error);
     return NextResponse.json({ message: "Failed to fetch product" }, { status: 500 });
