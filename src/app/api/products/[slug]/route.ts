0c1a1528b28384ff97105976f5379be3581c0ed3
Joseph Ahmed <ahmedjoseph11@gmail.com>
2025-12-11 18:14:31 +0600

    deployment related issue solved

--- src/app/api/products/[slug]/route.ts
+++ src/app/api/products/[slug]/route.ts
@@ -1,29 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
-import Product, { ProductDoc } from "@/models/Product";
+import Product from "@/models/Product";
 import { verifyFirebaseToken } from "@/lib/verifyToken";
 
-export const GET = async (
+export async function GET(
   req: NextRequest,
-  context: { params: { slug: string } }
-): Promise<NextResponse> => {
+  { params }: { params: Promise<{ slug: string }> }
+) {
+  const { slug } = await params;
   try {
     await connectDB();
-    // Verify token
-    const { valid, message } = await verifyFirebaseToken(req);
-    if (!valid) return NextResponse.json({ success: false, message: message! }, { status: 401 });
 
-    const { slug } = context.params; 
+    const { valid, message } = await verifyFirebaseToken(req);
+    if (!valid)
+      return NextResponse.json({ success: false, message }, { status: 401 });
 
     const product = await Product.findOne({ slug }).lean();
 
-    if (!product) {
+    if (!product)
       return NextResponse.json({ message: "Product not found" }, { status: 404 });
-    }
 
     return NextResponse.json(product);
   } catch (error) {
     console.error("Error fetching product:", error);
     return NextResponse.json({ message: "Failed to fetch product" }, { status: 500 });
   }
-};
+}
